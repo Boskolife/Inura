c975edb216e419f1e41451004db65d25bb178983
Boskolife <94291571+Boskolife@users.noreply.github.com>
2024-06-17 21:43:12 +0300

    add popUp

--- src/js/main.js
+++ src/js/main.js
@@ -4,39 +4,40 @@ openPopup();
 
 document.addEventListener('DOMContentLoaded', function () {
   const featuresItems = document.querySelectorAll('.item_name');
-  const swiper = new Swiper('.features_swiper', {
-    loop: true,
-    autoplay:true,
-    loopAddBlankSlides:false,
-    loopPreventsSliding:false,
-    slidesPerView: 1.7,
-    spaceBetween: 10,
-    centeredSlides: true,
-    slideToClickedSlide:true,
-    speed: 1500,
-    grabCursor: true,
-    breakpoints: {
-      768: {
-        slidesPerView: 5,
-        spaceBetween: 30,
+  const featuresSwiperContainer = document.querySelector('.features_swiper');
+  if (featuresSwiperContainer) {
+    const swiper = new Swiper('.features_swiper', {
+      loop: true,
+      autoplay: true,
+      loopAddBlankSlides: false,
+      loopPreventsSliding: false,
+      slidesPerView: 1.7,
+      spaceBetween: 10,
+      centeredSlides: true,
+      slideToClickedSlide: true,
+      speed: 1500,
+      grabCursor: true,
+      breakpoints: {
+        768: {
+          slidesPerView: 5,
+          spaceBetween: 30,
+        },
       },
-    },
 
-    on: {
-      slideChange: function () {
-        const activeIndex = this.realIndex;
-        featuresItems.forEach((item, index) => {
-          if (index === activeIndex) {
-            item.classList.add('item_active');
-          } else {
-            item.classList.remove('item_active');
-          }
-        });
+      on: {
+        slideChange: function () {
+          const activeIndex = this.realIndex;
+          featuresItems.forEach((item, index) => {
+            if (index === activeIndex) {
+              item.classList.add('item_active');
+            } else {
+              item.classList.remove('item_active');
+            }
+          });
+        },
       },
-    },
-  });
-
-  swiper.controller.control = swiperBtn;
+    });
+  }
 
   const updateActiveClass = () => {
     const activeIndex = swiper.realIndex;
@@ -66,59 +67,51 @@ document.addEventListener('DOMContentLoaded', function () {
   swiper.on('slideChange', updateActiveClass);
 });
 
-const swiperBtn = new Swiper('.btn_swiper', {
-  loop: true,
-  slidesPerView: 1,
-  centeredSlides: true,
-  speed: 0,
-  effect: 'slide',
-  simulateTouch: false,
-  on: {
-    transitionStart: function () {
-      this.setTransition(0);
-    },
-  },
-});
-
-const swiperCard = new Swiper('.cards_swiper', {
-  loop: true,
-  autoplay: true,
-  slidesPerView: 2.1,
-  spaceBetween: 10,
-  centeredSlides: true,
-  speed: 1500,
-  allowTouchMove: false,
-  breakpoints: {
-    768: {
-      slidesPerView: 5,
-      spaceBetween: 30,
-    },
-  },
-});
-
-if (window.innerWidth < 1024) {
-  const swiperCardReview = new Swiper('.testimonials_swiper', {
+const cardsSwiperContainer = document.querySelector('.cards_swiper');
+if (cardsSwiperContainer) {
+  const swiperCard = new Swiper('.cards_swiper', {
     loop: true,
-    loopAddBlankSlides: 3,
-    // autoplay: true,
-    slidesPerView: 1.7,
-    // spaceBetween: 10,
+    autoplay: true,
+    slidesPerView: 2.1,
+    spaceBetween: 10,
     centeredSlides: true,
     speed: 1500,
-    allowTouchMove: true,
-    pagination: {
-      el: '.swiper-pagination',
-    },
+    allowTouchMove: false,
     breakpoints: {
       768: {
-        slidesPerView: 3,
-        centeredSlides: false,
-      },
-      1024: {
-        slidesPerView: 4,
+        slidesPerView: 5,
+        spaceBetween: 30,
       },
     },
   });
+}
+
+if (window.innerWidth < 1024) {
+  const testimonialsSwiperContainer = document.querySelector(
+    '.testimonials_swiper',
+  );
+  if (testimonialsSwiperContainer) {
+    const swiperCardReview = new Swiper('.testimonials_swiper', {
+      loop: true,
+      loopAddBlankSlides: 3,
+      slidesPerView: 1.7,
+      centeredSlides: true,
+      speed: 1500,
+      allowTouchMove: true,
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 3,
+          centeredSlides: false,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+      },
+    });
+  }
 }
 
 function burgerMenu() {
@@ -143,23 +136,32 @@ function burgerMenu() {
 
 function openPopup() {
   const btn = document.querySelectorAll('.app_store_btn');
-  console.log(btn);
-  const popUp = document.querySelector('.app_store_popUp');
-  const popupContainer = document.querySelector('.popUp_container');
-  const closeElem = document.querySelector('.close_elem');
+  const popUp = document.querySelectorAll('.app_store_popUp');
+  const popupContainer = document.querySelectorAll('.popUp_container');
+  const closeElem = document.querySelectorAll('.close_elem');
 
   btn.forEach((item) => {
     item.addEventListener('click', (e) => {
       e.preventDefault();
-      popUp.classList.add('popUp_active');
-      popupContainer.style.display = 'block';
+      popUp.forEach((item) => {
+        item.classList.add('popUp_active');
+      });
+      popupContainer.forEach((item) => {
+        item.style.display = 'block';
+      });
       document.body.classList.add('popup_lock');
     });
   });
 
-  closeElem.addEventListener('click', () => {
-    popUp.classList.remove('popUp_active');
-    popupContainer.style.display = 'none';
-    document.body.classList.remove('popup_lock');
+  closeElem.forEach((item) => {
+    item.addEventListener('click', () => {
+      popUp.forEach((item) => {
+        item.classList.remove('popUp_active');
+      });
+      popupContainer.forEach((item) => {
+        item.style.display = 'none';
+      });
+      document.body.classList.remove('popup_lock');
+    });
   });
 }
